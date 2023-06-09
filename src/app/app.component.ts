42e8b368a8d02aa42cdd81743aca003aa53cb1c2
Alfie Farhana <alfie@hpe.com>
2023-06-09 18:24:47 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+/// <reference types="web-bluetooth" />
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-angular';
+	title = 'test-angular';
+	constructor() {
+	}
+	ngOnInit() {
+
+	}
+	async requestBluetooth() {
+		let bluetooth = navigator.bluetooth.requestDevice({ acceptAllDevices: true }).catch((error) => console.error(error))
+		console.log(bluetooth)
+	}
 }
